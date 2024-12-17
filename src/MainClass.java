b1926c5568ffce0a10af59bfec8d813b621d7d81
Michał Kolasa <michalkolasa90@gmail.com>
2024-12-17 12:31:11 +0100

    Update MainClass.java

--- src/MainClass.java
+++ src/MainClass.java
@@ -6,51 +6,51 @@ import java.util.Scanner;
 public class MainClass extends Polynomials {
 
     public static void main (String[] args) {
-        Polynomial w1 = new Polynomial();
-        Polynomial w2 = new Polynomial();
+        Polynomial p1 = new Polynomial();
+        Polynomial p2 = new Polynomial();
 
         Scanner scanner = new Scanner(System.in);
         Polynomials polynomials = new Polynomials();
 
         // Wczytanie i wypisanie wielomianu 1
         System.out.print("Please enter the degree of first polynomial: ");
-        w1.degree = scanner.nextInt();
+        p1.degree = scanner.nextInt();
 
-        for (int i = 0; i <= w1.degree; i++) {
+        for (int i = 0; i <= p1.degree; i++) {
             System.out.print("Coefficient at x^" + i + ": ");
-            w1.coefficients[i] = scanner.nextInt();
+            p1.coefficients[i] = scanner.nextInt();
         }
-        polynomials.printPolynomial(w1);
+        polynomials.printPolynomial(p1);
 
 
         // Wczytanie i wypisanie wielomianu 2
         System.out.print("Please enter the degree of second polynomial: ");
-        w2.degree = scanner.nextInt();
+        p2.degree = scanner.nextInt();
 
-        for (int i = 0; i <= w2.degree; i++) {
+        for (int i = 0; i <= p2.degree; i++) {
             System.out.print("Coefficient at x^" + i + ": ");
-            w2.coefficients[i] = scanner.nextInt();
+            p2.coefficients[i] = scanner.nextInt();
         }
-        polynomials.printPolynomial(w2);
+        polynomials.printPolynomial(p2);
 
 
         // Dodawanie wielomianów
         System.out.println();
-        Polynomial suma = polynomials.addition(w1, w2);
+        Polynomial suma = polynomials.addition(p1, p2);
         System.out.print("Sum of polynomials: ");
         polynomials.printPolynomial(suma);
         System.out.println();
 
 
         // Odejmowanie wielomianów
-        Polynomial roznica = polynomials.subtraction(w1, w2);
+        Polynomial roznica = polynomials.subtraction(p1, p2);
         System.out.print("Difference of polynomials: ");
         polynomials.printPolynomial(roznica);
         System.out.println();
 
 
         // Mnożenie wielomianów
-        Polynomial iloczyn = polynomials.multiplication(w1, w2);
+        Polynomial iloczyn = polynomials.multiplication(p1, p2);
         System.out.print("Product of polynomials: ");
         polynomials.printPolynomial(iloczyn);
         System.out.println();
@@ -60,6 +60,6 @@ public class MainClass extends Polynomials {
         double x;
         System.out.print("Please enter x for which the value of polynomial 1 is to be calculated: ");
         x = scanner.nextInt();
-        System.out.println("The value of the polynomial for x = " + x + ": " + polynomials.HornerAlgorithm(w1, x));
+        System.out.println("The value of the polynomial for x = " + x + ": " + polynomials.HornerAlgorithm(p1, x));
     }
 }
